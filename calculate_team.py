4fbcddc83e1e7800ece3cd820fe8d64da7c321b9
ccsaposs <ccsaposs+github@gmail.com>
2019-01-19 19:23:32 -1000

    Add calculate_team.py shell
    Calculations for a single Team.
    
    Team calculations include the calculation of data points that are
    reflective of a team's performance across all of their matches.
    
    Called by server.py with the number of the Team to be calculated.

---
+++ calculate_team.py
@@ -0,0 +1,37 @@
+"""Calculations for a single Team.
+
+Team calculations include the calculation of data points that are
+reflective of a team's performance across all of their matches.
+
+Called by server.py with the number of the Team to be calculated."""
+#!/usr/bin/python3.7
+# External imports
+import json
+import os
+import sys
+# Internal imports
+import utils
+
+# Check to ensure Team number is being passed as an argument
+if len(sys.argv) == 2:
+    # Extract Team number from system argument
+    TEAM_NUMBER = int(sys.argv[1])
+else:
+    print('Error: Team number not being passed as an argument. Exiting...')
+    sys.exit(0)
+
+#TODO: Open TIMD data
+
+#TODO: Do calculations
+
+FINAL_TEAM_DATA = {}
+
+# Save data in local cache
+with open(utils.create_file_path('data/teams/' + TEAM_NUMBER + '.json'),
+          'w') as file:
+    json.dump(FINAL_TEAM_DATA, file)
+
+# Save data in Firebase upload queue
+with open(utils.create_file_path('data/upload_queue/teams' + TEAM_NUMBER
+                                 + '.json'), 'w') as file:
+    json.dump(FINAL_TEAM_DATA, file)
