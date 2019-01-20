eab696cc582bdf6b889fa4b036fd199b790321fb
ccsaposs <ccsaposs+github@gmail.com>
2019-01-19 18:50:33 -1000

    Update calculate_timd.py file docstring

--- calculate_timd.py
+++ calculate_timd.py
@@ -1,4 +1,19 @@
-"""Updates assignments when the cycle number is changed."""
+"""Calculations for a single TIMD.
+
+TIMD stands for Team In Match Data.  TIMD calculations include
+consolidation of (up to) 3 tempTIMDs (temporary TIMDs) into a single
+TIMD, and the calculation of data points that are reflective of a team's
+performance in a single match.
+
+Consolidation is the process of determining the actions a robot
+performed in a match by using data from (up to) 3 tempTIMDs.  One
+tempTIMD is created per scout per match.  Ideally, 18 scouts are
+distributed evenly across the 6 robots per match, resulting in 3
+tempTIMDs per robot per match (aka 3 tempTIMDs per TIMD).  However, the
+number of tempTIMDs per TIMD may be less than 3, depending on scout
+availability, incorrect scout distribution, or missing data.
+
+Called by server.py with the name of the TIMD to be calculated."""
 #!/usr/bin/python3.7
 # External imports
 import json
