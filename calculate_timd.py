d26fc11e771af0fd2df24803d99c87933add76f4
EThan1678 <ethangtalbert@gmail.com>
2019-01-21 15:43:54 -0800

    Add support for non-timed key consolidation

--- calculate_timd.py
+++ calculate_timd.py
@@ -20,7 +20,7 @@ import json
 import sys
 # Internal imports
 import utils
-
+'''
 # Check to ensure TIMD name is being passed as an argument
 if len(sys.argv) == 2:
     # Extract TIMD name from system argument
@@ -28,12 +28,306 @@ if len(sys.argv) == 2:
 else:
     print('Error: TIMD name not being passed as an argument. Exiting...')
     sys.exit(0)
-
+'''
+TIMD_NAME = 't'
 #TODO: Open tempTIMD data
 
-#TODO: Do calculations
+#TODO: Add decompression to decompress timds before they are calculated
+
+# CONSOLIDATION OF TEMPTIMDS
+
+#TODO: Add implementation for TEMP_TIMDS and sprking
+SPRKING = 0
+TEMP_TIMDS = {'h' : {
+    'startingLevel': 2,
+    'crossedHabLine': True,
+    'startingLocation': 'left',
+    'preload' : 'lemon',
+    'driverStation': 1,
+    'isNoShow': False,
+    'timerStarted': 1547528330,
+    'currentCycle': 4,
+    'scoutID': 7,
+    'scoutName': 'Carl',
+    'appVersion': '1.2',
+    'assignmentMode': 'QR',
+    'assignmentFileTimestamp': 1547528290,
+    'matchesNotScouted': [1, 14, 28, 35],
+    'timeline': [
+        {
+            'type': 'intake',
+            'time' : '102.4',
+            'piece': 'orange',
+            'zone': 'rightLoadingStation',
+            'didSucceed': True,
+            'wasDefended': False,
+        },
+        {
+            'type': 'incap',
+            'time': '109.6',
+            'cause': 'brokenMechanism',
+        },
+        {
+            'type': 'unincap',
+            'time': '111.1',
+        },
+        {
+            'type': 'drop',
+            'time': '112.1',
+            'piece': 'orange',
+        },
+        {
+            'type': 'intake',
+            'time': '120',
+            'piece': 'lemon',
+            'zone': 'zone2Left',
+            'didSucceed': True,
+            'wasDefended': True,
+        },
+        {
+            'type': 'placement',
+            'time': '127.4',
+            'piece': 'lemon',
+            'didSucceed': False,
+            'wasDefended': False,
+            'structure': 'leftRocket',
+            'side': 'right',
+            'level': 2,
+        },
+        {
+            'type': 'spill',
+            'time': '130',
+            'piece': 'lemon',
+        },
+        {
+            'type': 'climb',
+            'time': '138',
+            'attempted': {'self': 3, 'robot1': 3, 'robot2': 2},
+            'actual': {'self': 3, 'robot1': 2, 'robot2': 1},
+        }
+    ],
+}, 'r' : {
+    'startingLevel': 1,
+    'crossedHabLine': None,
+    'startingLocation': 'mid',
+    'preload' : 'lemon',
+    'driverStation': 1,
+    'isNoShow': False,
+    'timerStarted': 1547528330,
+    'currentCycle': 4,
+    'scoutID': 7,
+    'scoutName': 'Carl',
+    'appVersion': '1.2',
+    'assignmentMode': 'QR',
+    'assignmentFileTimestamp': 1547528290,
+    'matchesNotScouted': [1, 14, 28, 35],
+    'timeline': [
+        {
+            'type': 'intake',
+            'time' : '102.4',
+            'piece': 'orange',
+            'zone': 'rightLoadingStation',
+            'didSucceed': True,
+            'wasDefended': False,
+        },
+        {
+            'type': 'incap',
+            'time': '109.6',
+            'cause': 'brokenMechanism',
+        },
+        {
+            'type': 'unincap',
+            'time': '111.1',
+        },
+        {
+            'type': 'drop',
+            'time': '112.1',
+            'piece': 'orange',
+        },
+        {
+            'type': 'intake',
+            'time': '120',
+            'piece': 'lemon',
+            'zone': 'zone2Left',
+            'didSucceed': True,
+            'wasDefended': True,
+        },
+        {
+            'type': 'placement',
+            'time': '127.4',
+            'piece': 'lemon',
+            'didSucceed': False,
+            'wasDefended': False,
+            'structure': 'leftRocket',
+            'side': 'right',
+            'level': 2,
+        },
+        {
+            'type': 'spill',
+            'time': '130',
+            'piece': 'lemon',
+        },
+        {
+            'type': 'climb',
+            'time': '138',
+            'attempted': {'self': 3, 'robot1': 3, 'robot2': 2},
+            'actual': {'self': 3, 'robot1': 2, 'robot2': 1},
+        }
+    ],
+}, 'g' : {
+    'startingLevel': 3,
+    'crossedHabLine': None,
+    'startingLocation': 'left',
+    'preload' : 'lemon',
+    'driverStation': 1,
+    'isNoShow': False,
+    'timerStarted': 1547528330,
+    'currentCycle': 4,
+    'scoutID': 7,
+    'scoutName': 'Carl',
+    'appVersion': '1.2',
+    'assignmentMode': 'QR',
+    'assignmentFileTimestamp': 1547528290,
+    'matchesNotScouted': [1, 14, 28, 35],
+    'timeline': [
+        {
+            'type': 'intake',
+            'time' : '102.4',
+            'piece': 'orange',
+            'zone': 'rightLoadingStation',
+            'didSucceed': True,
+            'wasDefended': False,
+        },
+        {
+            'type': 'incap',
+            'time': '109.6',
+            'cause': 'brokenMechanism',
+        },
+        {
+            'type': 'unincap',
+            'time': '111.1',
+        },
+        {
+            'type': 'drop',
+            'time': '112.1',
+            'piece': 'orange',
+        },
+        {
+            'type': 'intake',
+            'time': '120',
+            'piece': 'lemon',
+            'zone': 'zone2Left',
+            'didSucceed': True,
+            'wasDefended': True,
+        },
+        {
+            'type': 'placement',
+            'time': '127.4',
+            'piece': 'lemon',
+            'didSucceed': False,
+            'wasDefended': False,
+            'structure': 'leftRocket',
+            'side': 'right',
+            'level': 2,
+        },
+        {
+            'type': 'spill',
+            'time': '130',
+            'piece': 'lemon',
+        },
+        {
+            'type': 'climb',
+            'time': '138',
+            'attempted': {'self': 3, 'robot1': 3, 'robot2': 2},
+            'actual': {'self': 3, 'robot1': 2, 'robot2': 1},
+        }
+    ],
+}}
 
 FINAL_TIMD = {}
+
+# When consolidating non-timed keys, it is easy to consolidate them,
+# as you can simply find which value is the most common in the set of
+# three possibilities.
+
+# Iterates through the keys of the first tempTIMD and finds the ones
+# that are not timed or scout diagnostics.
+for data_field in list(TEMP_TIMDS[SPRKING]):
+    if data_field not in ['timeline',
+                          'timerStarted',
+                          'currentCycle',
+                          'scoutID',
+                          'scoutName',
+                          'appVersion',
+                          'assignmentMode',
+                          'assignmentFileTimestamp',
+                          'matchesNotScouted']:
+
+        # Creates a dictionary of each scout to the key from their
+        # tempTIMD to compare against each other. (Code note - This code
+        # is using .get and not simply referencing the key out of the
+        # dictionary because .get doesn't error out when the key doesn't
+        # exist. It instead returns NoneType).
+        data_field_comparison_list = {scout : dicti.get(data_field) for scout, dicti in
+                                      TEMP_TIMDS.items() if dicti.get(data_field) is not None}
+
+        # If there are NoneTypes in the list, truncnate them, they mess
+        # up later calculation. However, if they all are None, the data
+        # point is missing,
+        # Creates a dictionary with how many times an item appeared in
+        # the comparison list.
+        occurence_list = {data_field : data_field_comparison_list.count(data_field)
+                          for data_field in set(data_field_comparison_list)}
+
+        # If the highest occurence on the occurence list is the same as
+        # the lowest occurence, the correct value for the datapoint is
+        # the value output by the scout with the best spr. This triggers
+        # both when all the scout values are the same (The max and min
+        # would both be three) and when all the scout values are
+        # different (The max and min are both 1). In the case of any
+        # other scenario, the max is trusted because it would suggest
+        # the max is the 2 in a 2 scout versus 1 split decision.
+        if max(occurence_list.values()) == min(occurence_list.values()):
+            FINAL_TIMD[data_field] = TEMP_TIMDS[SPRKING][data_field]
+        else:
+            FINAL_TIMD[data_field] = max(occurence_list, key=occurence_list.get)
+    # If the data field name is not any of the non-timed data keys or
+    # the timeline (Which is computed later) it shouldn't be
+    # consolidated. This group of data fields in the tempTIMDs is
+    # mostly scout diagnostics.
+    elif data_field != 'timeline':
+        pass
+    # If the data field doesn't fit any of the previous requirements,
+    # it must be the timeline.
+    else:
+        # In order to compute the timeline properly, it is split into
+        # a list of the timelines.
+        timelines = [temp_timd['timeline'] for temp_timd in TEMP_TIMDS]
+
+        # If the list of timelines only includes one timeline, that
+        # timeline is taken as the correct one and put into the final
+        # TIMD.
+        if len(timelines) == 1:
+            FINAL_TIMD['timeline'] = timelines[0]
+        #TODO: Program case of 2 tempTIMDs
+        
+        # If the list has three tempTIMDs, the process for computation
+        # is more complicated.
+        else:
+             
+
+print(FINAL_TIMD)
+
+
+
+
+
+
+
+
+'''
+
+
 
 # Save data in local cache
 with open(utils.create_file_path('data/timds/' + TIMD_NAME + '.json'),
@@ -44,3 +338,4 @@ with open(utils.create_file_path('data/timds/' + TIMD_NAME + '.json'),
 with open(utils.create_file_path('data/upload_queue/timds' + TIMD_NAME +
                                  '.json'), 'w') as file:
     json.dump(FINAL_TIMD, file)
+'''
