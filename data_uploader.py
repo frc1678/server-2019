9df67980daab16f88b1edd77acb9384097423ce5
EThan1678 <ethangtalbert@gmail.com>
2019-02-02 09:24:15 -0800

    Modify comments and variables to be descriptive.

--- data_uploader.py
+++ data_uploader.py
@@ -1,8 +1,7 @@
-"""Uploads data to the firebase from the cached data files inside the
-server-2019 folder.
+"""Uploads data from files in the 'upload_queue' directory to firebase.
 
 Searches through the 'upload_queue' directory for data that needs to be
-uploaded since its last calculation, and sends it to firebase.
+uploaded and sends it to firebase.
 """
 
 # External imports
@@ -13,19 +12,21 @@ import firebase_communicator
 import utils
 
 # Uses default firebase URL
+# DB stands for database
 DB = firebase_communicator.configure_firebase()
 
 def collect_file_data(data_file, root_key):
-    """Collects data from the data_file and returns it.
+    """Organizes each data point from the data_file as a firebase path.
 
     Takes data from the file passed as an argument, and forms each of
     the data points inside the file into a pathway added to a dictionary
-    which is returned. The passed argument data_file is the path of the
-    specific file that data is taken from. root_key is the basic key on
-    the firebase in which the data is eventually sent, limited to only
+    which is returned.
+    The passed argument data_file is the path of the specific file that
+    data is taken from. root_key is the basic key on the firebase in
+    which the data is eventually sent, limited to only
     'Teams', Matches', and 'TeamInMatchDatas'. """
-    with open(data_file, 'r') as file_data:
-        file_data = json.load(file_data)
+    with open(data_file, 'r') as upload_queue_file_data:
+        file_data = json.load(upload_queue_file_data)
 
     # Defines the file name for to use when defining keys in the
     # path_data dictionary.
