18dcd21591f0e18b62864cae014eb8426fdabdbb
ccsaposs <ccsaposs+github@gmail.com>
2019-02-02 13:40:52 -1000

    Add export_data.py

---
+++ export_data.py
@@ -0,0 +1,6 @@
+"""Exports Team and TIMD data to CSV files for picklist spreadsheet.
+
+Team data is saved in 'data/exports/export-TEAM.csv'
+TIMD data is saved in 'data/exports/export-TIMD.csv'
+The picklist spreadsheet is in Google Sheets.  Each CSV file is imported
+into a different sheet."""
