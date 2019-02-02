8f9e619e19f33d605e858f59cb70deb527ae6a52
EThan1678 <ethangtalbert@gmail.com>
2019-02-02 14:26:34 -0800

    Add a statement to remove used upload_queue files

--- data_uploader.py
+++ data_uploader.py
@@ -61,5 +61,8 @@ for firebase_key, cache_key in FIREBASE_TO_CACHE_KEY.items():
         # Collects and adds the data from a single file to 'FINAL_DATA'
         FINAL_DATA.update(collect_file_data(file_path, firebase_key))
 
+        # Removes the file so that the same data isn't uploaded more than once.
+        os.remove(file_path)
+
 # Sends the data to firebase.
 DB.update(FINAL_DATA)
