94807b2965b09cdc40f6c75fc64adc8ba94b4249
ccsaposs <ccsaposs+github@gmail.com>
2019-02-05 21:48:25 -0800

    Add request_match_schedule()

--- tba_communicator.py
+++ tba_communicator.py
@@ -26,3 +26,34 @@ def make_request(api_url, last_modified_header=None):
     if last_modified_header is not None:
         request_headers['If-Modified-Since'] = last_modified_header
     return requests.get(full_url, headers=request_headers)
+
+LAST_MATCH_SCHEDULE_REQUEST = {}
+def request_match_schedule():
+    """Requests the match schedule from the TBA API."""
+    print('Retrieving data from TBA...')
+    while True:
+        try:
+            request = make_request(f'event/{EVENT_CODE}/matches/simple', \
+                LAST_MATCH_SCHEDULE_REQUEST.get('Last-Modified'))
+        except requests.exceptions.ConnectionError:
+            print('Error: No internet connection.  Trying again in 3 seconds...')
+        else:
+            print('TBA data successfully retrieved.')
+            break
+        time.sleep(3)
+
+    # A 304 status code means the data was not modified since our last
+    # request, and we can pull it from the cache.
+    if request.status_code == 304:
+        return LAST_MATCH_SCHEDULE_REQUEST['data']
+    # A 200 status code means the request was successful
+    elif request.status_code == 200:
+        # Updates local cache
+        LAST_MATCH_SCHEDULE_REQUEST['Last-Modified'] = \
+            request.headers['Last-Modified']
+        LAST_MATCH_SCHEDULE_REQUEST['data'] = request.json()
+
+        return request.json()
+    else:
+        print(f'Request failed with status code:{request.status_code}')
+        return {}
