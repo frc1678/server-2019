1a162be0bafcafcf1610d3242486497b7f333369
Carl Csaposs <32380359+ccsaposs@users.noreply.github.com>
2019-02-10 14:19:23 -0800

    Update file name in server.py subprocess call (#23)

--- server.py
+++ server.py
@@ -63,7 +63,7 @@ def cycle_num_stream_handler(snapshot):
         cycle_number = snapshot['data']
         if cycle_number is None:
             cycle_number = 0
-        subprocess.call('python3 updateAssignments.py ' +
+        subprocess.call('python3 update_assignments.py ' +
                         str(cycle_number), shell=True)
 
 def temp_timd_stream_handler(snapshot):
