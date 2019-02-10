a22a3ca1b4fe234c407a9b3e74f164d0f7a8fd63
Nathan S <nathansolomon1678@gmail.com>
2019-02-10 15:51:46 -0800

    Figure out command for adb push to specific device and how to list connected devices

--- file_sender.py
+++ file_sender.py
@@ -5,8 +5,14 @@ import subprocess
 path = utils.create_file_path("data/assignments/assignments.json", True)
 # Continuously load files on to the tablet
 # so that it gets loaded on whenever a tablet is plugged in.
-# TODO(Someone): figure out how to get a confirmation message
+#TODO(Someone): figure out how to get a confirmation message
 # when the file is received.
 while (True):
-    subprocess.call(f"adb push '{path}' '/home/main_storage/bluetooth'",
-                    shell=True)
+    output = subprocess.check_output("adb devices".split(" "))
+    print(output)
+    #TODO(Nathan) figure out hot to get list of serial numbers
+    # ("devices") from command output ("output")
+    ##################for device in devices:
+    ##################    subprocess.call(f"adb -s {device} push '{path}'
+    ##################                     '/mnt/sdcard/bluetooth'",
+    ##################                    shell=True)
