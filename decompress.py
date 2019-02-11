77213a5d3e145124ac7850df2ca320846c6cc1e4
mckennalincoln <43869831+mckennalincoln@users.noreply.github.com>
2019-02-10 16:42:50 -0800

    Add tempTIMD timeline decompression (#1)
    * Decompress tempTIMD timeline
    
    * Fixes the problem with the climb dictionaries.
    
    Changes code for dealing with the climb dictionaries. Removes the code for
    splitting the data at the colens. Changes variable names for the decompression
    dictionaries. Fixes formatting for the doc string.
    
    * Updates the dictionaries used for decompression.
    
    Adds new data points into the compression keys and values.
    
    * Fixes the line length formatting.
    
    Shortens the comments and the lines of code so they are not over the limit.
    
    * Adds code to deal with no show teams.
    
    Adds an "if not" statement to catch if a team does not have any timeline data.
    
    * Fixes the length of the lines.
    
    Shortens the length of the comments and the lenth of lines of code.
    
    * Changes name of function and argument.
    
    Changes to remove capitals from function names so they conform to snake-case
    style.
    
    * Fixes the merge conflict fixes that were not saved.
    
    Removes the code for the merge conflicts that was push to git hub.
    
    * Updates the comments.
    
    Updates comments with periods and new function name.
    
    * Adds return statement to the function.
    
    * Adds decompress_temp_timd function.
    
    Function calls timeline and header functions. Takes in the compressed
    tempTIMD and outputs a dictionary of the decompressed tempTIMD.
    
    * Removes code concerning the underscore.
    
    Removes the code checking for the underscore and the split at the underscore.
    
    * Fixes syntax error for decompress_temp_timd function.
    
    Rearanges the function so it returns the full decompressed temp timd.
    
    * Updates file doc string.
    
    * Updates and fixes problems in doc strings.
    
    * Fixes doc strings.
    
    * Changes variable names.
    
    * Fixed formatting in doc string for tempTIMD timeline decompression.
    
    * Changes variable name for splitting by commas.
    
    * Changes variable name from char to character.
    
    * Changes comments to make more clear.
    
    * Moves decompression dictionaries outside of function.
    
    * Modify tempTIMD timeline compression (#4)
    
    * Update docstrings in decompress.py
    
    * Rename 'action_dic' to 'decompressed_action'
    
    * Separate one-line statement into three lines
    
    * Simplify climb if-statement
    
    * Rename 'climb_dic' to 'decompressed_climb'
    
    * Change tuple in if-statement to list
    
    * Separate one-line statement into three lines
    
    * Simplify climb decompression
    
    * Fix continuned indentation
    
    * Update comments in decompress.py
    
    * Fixes mistake in TEMP_TIMD_COMPRESSION_KEYS dictionary.
    
    * Add int type converting + re-structure logic (#6)
    
    * Use 'isalpha()' instead of 'ascii_letters'
    
    * Add int type converting + re-structure logic
    
    * Debug tempTIMD timeline decompression
    
    Add handling for trailing comma
    Add handling for the first key of an action
    Save a key's index and value after saving the previous key:value pair
    Remove curly brackets before iterating through climb

---
+++ decompress.py
@@ -0,0 +1,203 @@
+"""Decompresses tempTIMD data.
+
+Called by calculate_timd.py."""
+# No external imports
+# No internal imports
+
+# Compressed tempTIMD key to uncompressed tempTIMD key
+TEMP_TIMD_COMPRESSION_KEYS = {
+    'a': 'startingLevel',
+    'b': 'crossedHabLine',
+    'c': 'startingLocation',
+    'd': 'preload',
+    'e': 'driverStation',
+    'f': 'isNoShow',
+    'g': 'timerStarted',
+    'h': 'currentCycle',
+    'j': 'scoutID',
+    'k': 'scoutName',
+    'm': 'appVersion',
+    'n': 'assignmentMode',
+    'p': 'assignmentFileTimestamp',
+    'q': 'matchesNotScouted',
+    'r': 'type',
+    's': 'time',
+    't': 'piece',
+    'u': 'zone',
+    'v': 'didSucceed',
+    'w': 'wasDefended',
+    'H': 'shotOutOfField',
+    'x': 'structure',
+    'y': 'side',
+    'z': 'level',
+    'A': 'cause',
+    'B': 'attempted',
+    'C': 'actual',
+    'D': 'self',
+    'E': 'robot1',
+    'F': 'robot2',
+}
+# Compressed tempTIMD value to uncompressed tempTIMD value
+TEMP_TIMD_COMPRESSION_VALUES = {
+    'T': True,
+    'F': False,
+    'A': 'left',
+    'B': 'mid',
+    'C': 'right',
+    'D': 'far',
+    'E': 'orange',
+    'G': 'lemon',
+    'H': 'none',
+    'J': 'QR',
+    'K': 'backup',
+    'L': 'override',
+    'M': 'intake',
+    'N': 'placement',
+    'P': 'drop',
+    'Q': 'spill',
+    'R': 'climb',
+    'S': 'incap',
+    'U': 'unincap',
+    't': 'startDefense',
+    'u': 'endDefense',
+    'V': 'zone1Left',
+    'W': 'zone1Right',
+    'X': 'zone2Left',
+    'Y': 'zone2Right',
+    'Z': 'zone3Left',
+    'a': 'zone3Right',
+    'b': 'zone4Left',
+    'c': 'zone4Right',
+    'd': 'leftLoadingStation',
+    'e': 'rightLoadingStation',
+    'f': 'leftRocket',
+    'g': 'rightRocket',
+    'h': 'cargoShip',
+    'j': 'tippedOver',
+    'k': 'brokenMechanism',
+    'm': 'stuckOnObject',
+    'n': 'stuchOnHab',
+    'p': 'emergencyStop',
+    'q': 'noControl',
+    'r': 'twoGamePieces',
+    's': 'near',
+}
+
+def decompress_timeline_value(compressed_value):
+    """Decompresses a single tempTIMD timeline action value.
+
+    compressed_value is a string."""
+    # If the 'compressed_value' is a one-character letter, it needs to
+    # be decompressed.
+    if len(compressed_value) == 1 and compressed_value.isalpha():
+        return TEMP_TIMD_COMPRESSION_VALUES[compressed_value]
+    # Checks if 'compressed_value' only contains characters 0-9
+    elif compressed_value.isdigit():
+        # 'compressed_value' is an integer
+        return int(compressed_value)
+    # Otherwise, 'compressed_value' is a timestamp (which can contain
+    # '*' characters, and should be left as a string) or
+    # 'compressed_value' is a climb dictionary, which is handled later.
+    else:
+        return compressed_value
+
+def decompress_temp_timd_timeline(compressed_temp_timd_timeline):
+    """Decompresses a single tempTIMD timeline.
+
+    Decompresses a tempTIMD timeline to a list of dictionaries.
+    Each dictionary represents an action in the timeline.
+
+    compressed_temp_timd_timeline is a string."""
+    if compressed_temp_timd_timeline[-1] == ',':
+        # In case of an extra comma in the headers it removes them
+        compressed_temp_timd_timeline = compressed_temp_timd_timeline[:-1]
+    compressed_temp_timd_timeline = compressed_temp_timd_timeline.split(',')
+    decompressed_timeline = []
+    for action in compressed_temp_timd_timeline:
+        decompressed_action = {}
+        # Index of the last key in the action.
+        index_last_key = 0
+        inside_curly_bracket = False
+
+        for index, character in enumerate(action):
+            if character == '{':
+                inside_curly_bracket = True
+            elif character == '}':
+                inside_curly_bracket = False
+            if inside_curly_bracket is True:
+                # Ignore characters in curly brackets (e.g. climb), and
+                # deal with them later.
+                pass
+            # The character is the last character in the string
+            elif index == len(action)-1:
+                # Decompress the previous key and value
+                decompressed_key = TEMP_TIMD_COMPRESSION_KEYS[key]
+                # The previous value is from the last key to the end of
+                # the string.
+                compressed_value = action[index_last_key+1:index+1]
+                decompressed_value = decompress_timeline_value(
+                    compressed_value)
+
+                # Save the previous key:value pair in the final dictionary.
+                decompressed_action[decompressed_key] = decompressed_value
+            # Special handling for the first key
+            elif character.isalpha() and index == 0:
+                index_last_key = 0
+                key = character
+            # The character is a key if it is more than one character
+            # after the last key and it is a letter.
+            elif character.isalpha() and (index - index_last_key > 1):
+                # Decompress the previous key and value
+                decompressed_key = TEMP_TIMD_COMPRESSION_KEYS[key]
+                # The previous value is between the last key and the
+                # current key.
+                compressed_value = action[index_last_key+1:index]
+                decompressed_value = decompress_timeline_value(
+                    compressed_value)
+
+                # Save the previous key:value pair in the final dictionary.
+                decompressed_action[decompressed_key] = decompressed_value
+
+                # Save the index and value of the key
+                index_last_key = index
+                key = character
+
+        # Special case to deal with the data from the climb.
+        # Finds which action dictionary has climb in it.
+        if decompressed_action.get('type') == 'climb':
+            for climb_key in ['attempted', 'actual']:
+                # 'climb_items' example format: ['D3', 'E3', 'F0']
+                # [1:-1] removes curly brackets
+                climb_items = decompressed_action[climb_key][1:-1].split(';')
+                decompressed_climb = {}
+                for climb_item in climb_items:
+                    decompressed_key = TEMP_TIMD_COMPRESSION_KEYS[
+                        climb_item[0]]
+                    decompressed_value = int(climb_item[1])
+                    decompressed_climb[decompressed_key] = decompressed_value
+                decompressed_action[climb_key] = decompressed_climb
+
+        decompressed_timeline.append(decompressed_action)
+    return decompressed_timeline
+
+def decompress_temp_timd(compressed_temp_timd):
+    """Decompresses a single tempTIMD.
+
+    Calls decompress_temp_timd_headers() and
+    decompress_temp_timd_timeline().
+    The headers are the non-timed data fields and are stored in a
+    dictionary.  The timeline is a list of dictionaries.  Each
+    dictionary represents a timed action.  Returns a dictionary.
+
+    compressed_temp_timd is a string."""
+    compressed_header = compressed_temp_timd.split('_')[0]
+    compressed_timeline = compressed_temp_timd.split('_')[1]
+
+    decompressed_temp_timd = {}
+
+    decompressed_temp_timd.update(decompress_temp_timd_headers(
+        compressed_header))
+    decompressed_temp_timd['timeline'] = decompress_temp_timd_timeline(
+        compressed_timeline)
+
+    return decompressed_temp_timd
