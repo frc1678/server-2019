8b82d137483c3ccb5ad20aa195f22f81cc3a96f4
Nathan S <nathansolomon1678@gmail.com>
2019-02-10 16:45:34 -0800

    Finish file_sender.py Still has to be tested to see if it works

--- file_sender.py
+++ file_sender.py
@@ -8,11 +8,15 @@ path = utils.create_file_path("data/assignments/assignments.json", True)
 #TODO(Someone): figure out how to get a confirmation message
 # when the file is received.
 while (True):
-    output = subprocess.check_output("adb devices".split(" "))
-    print(output)
+    output = str(subprocess.check_output("adb devices".split(" ")))
+    output = output.split("\\n")[1:-2]
+    # serial is a 16 character string
+    devices = list(map(lambda word : word[:16],
+                       output))
     #TODO(Nathan) figure out hot to get list of serial numbers
     # ("devices") from command output ("output")
-    ##################for device in devices:
-    ##################    subprocess.call(f"adb -s {device} push '{path}'
-    ##################                     '/mnt/sdcard/bluetooth'",
-    ##################                    shell=True)
+    for device in devices:
+        subprocess.call(f"adb -s {device} push '{path}' '/mnt/sdcard/bluetooth'",
+                        shell=True)
+        print(f"Loaded file \"{path}\" onto tablet with serial {device}")
+    print("\n")
