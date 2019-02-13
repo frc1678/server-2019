7157a714fb27f62b3497e159722c70d8946786e8
ccsaposs <ccsaposs+github@gmail.com>
2019-02-12 17:00:52 -0800

    Add user confirmation

--- prepare_firebase.py
+++ prepare_firebase.py
@@ -5,7 +5,16 @@
 import firebase_communicator
 import tba_communicator
 
-# TODO: Add user confirmation, warn that Firebase will be wiped
+# User confirmation
+while True:
+    print('\nWARNING: Firebase will be wiped!')
+    CONFIRMATION = input("Type 'yes' to continue: ")
+    # Uses '.lower()' to be case-insensitive
+    if CONFIRMATION.lower() == 'yes':
+        break
+    else:
+        print('Press CTRL+C to exit')
+
 
 # TODO: Wipe certain Firebase collections
 # TODO: Populate Firebase with Teams and Matches
