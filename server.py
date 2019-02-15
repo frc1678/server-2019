26006951ada8ad345ff8da91f1c5a7d29bca85ce
Carl Csaposs <32380359+ccsaposs@users.noreply.github.com>
2019-02-14 17:40:07 -0800

    Fix file path bug in server.py (#36)
    * Fix file path bug in server.py
    
    * Update comment in server.py

--- server.py
+++ server.py
@@ -99,12 +99,12 @@ def temp_timd_stream_handler(snapshot):
         # and we should delete it from our local copy.
         if temp_timd_value is None:
             os.remove(utils.create_file_path(
-                f'data/cache/temp_timds{temp_timd_name}.txt'))
+                f'data/cache/temp_timds/{temp_timd_name}.txt'))
             # Causes the corresponding TIMD to be recalculated
             register_modified_temp_timd(temp_timd_name)
         else:
             with open(utils.create_file_path(
-                    f'data/cache/temp_timds{temp_timd_name}.txt'),
+                    f'data/cache/temp_timds/{temp_timd_name}.txt'),
                       'w') as file:
                 file.write(temp_timd_value)
             timd_name = temp_timd_name.split('-')[0]
@@ -183,7 +183,7 @@ while True:
     # Checks list of tempTIMDs from files to determine what calculations
     # are needed.
 
-    # List of files (tempTIMDs) in the 'data' directory.
+    # List of files (tempTIMDs) in the 'temp_timds' cache directory.
     TEMP_TIMD_FILES = os.listdir(utils.create_file_path(
         'data/cache/temp_timds'))
     # Stores groups of matching tempTIMDs under a single key (which is
