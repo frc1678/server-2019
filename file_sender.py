acda3d97fe68d3efb8bd47fdd13d844ff5536c5b
Apurva Mishra <apurvagyan@gmail.com>
2019-02-16 11:14:29 -0800

    Pylint formatting "Your code has been rated at 9.70/10"

--- file_sender.py
+++ file_sender.py
@@ -9,7 +9,7 @@ import time
 import utils
 
 # serial number to human-readable name of device
-serials_dict = {
+SERIALS_DICT = {
     # Green case tablets (Scouts 1-8)
     '094d5740': 'Scout 1',
     '0904a28b': 'Scout 2',
@@ -35,90 +35,90 @@ serials_dict = {
     'G000H4056383066L': 'Backup 2',
     'G000H40563460T65': 'Backup 3',
     'G000H404610600EK': 'Backup 4',
-    'G0K0KH02623400GT': 'Backup 5'
+    'G0K0KH02623400GT': 'Backup 5',
     # this does not include serial numbs for super scouting tablets
     # (those are the ones named 'red', 'blue', & 'purple')
 }
 
-assignment_file_path = utils.create_file_path(
-                       'data/assignments/assignments.txt', True)
+ASSIGNMENT_FILE_PATH = utils.create_file_path(
+    'data/assignments/assignments.txt')
 
 # list of devices with the file so we do not copy the file
 # onto the device again
-devices_with_file = []
+DEVICES_WITH_FILE = []
 
-""" Makes sure that (1) the file exists and
-(2) that file is the same as assignments.txt """
-def file_load_success(device):
+def file_load_success(device_id):
+    """ Makes sure that (1) the file exists and
+    (2) that file is the same as assignments.txt """
     try:
-        # pull tablet's copy of the device into pulled_copy.txt
+        # pull tablet's copy of assignments.txt into pulled_copy.txt
         # then compare pulled_copy.txt to assignments.txt
         # if they're the same then it was pushed properly
 
         # remove everything after last slash
-        copy_file_path = assignment_file_path.split("/")[:-1]
+        copy_file_path = ASSIGNMENT_FILE_PATH.split("/")[:-1]
         copy_file_path.append('pulled_copy.txt')
         copy_file_path = '/'.join(copy_file_path)
         # use 'utils.create_file_path' to create the file if it doesn't exist
         copy_file_path = utils.create_file_path(copy_file_path, True)
         subprocess.call(f'touch {copy_file_path}'.split(' '))
 
-        subprocess.call(f'adb -s {device} pull /mnt/sdcard/bluetooth/assignments.txt {copy_file_path}'
+        subprocess.call(f'adb -s {device_id} pull /mnt/sdcard/bluetooth/assignments.txt {copy_file_path}'
                         .split(' '))
         # the 'r' option for open() indicates that
         # the file will only be used for reading from
         with open(copy_file_path, 'r') as file:
             copy_data = file.read()
-        with open(assignment_file_path, 'r') as file:
+        with open(ASSIGNMENT_FILE_PATH, 'r') as file:
             assignment_data = file.read()
         return copy_data == assignment_data
     except FileNotFoundError:
         return False
 
 while True:
-    # stores output from 'adb devices' command in var 'output'
+    # stores output from 'adb devices' command in var 'OUTPUT'
     # 'adb devices' finds serials of all connected devices (USB cable)
 
-    # Here's and example output of 'adb devices':
+    # Here's an example output of 'adb devices':
     # List of devices attached
     # 015d2568753c1408	device
     # 015d2856d607f015	device
-    output = str(subprocess.check_output('adb devices'.split(' ')))
+    OUTPUT = str(subprocess.check_output('adb devices'.split(' ')))
     # check_output returns a byte string and converting that to a string
-    # makes it weird, so here's what output looks like now:
+    # makes it weird, so here's what OUTPUT looks like now:
     # "b'List of devices attached\\n015d2568753c1408\\tdevice\\n015d2856d607f015\\tdevice\\n\\n'"
 
-    # now split output values at each '\\n' so that output looks like:
+    # now split OUTPUT values at each '\\n' so that OUTPUT looks like:
     # ["b'List of devices attached", '015d2568753c1408\\tdevice',
     #  '015d2856d607f015\\tdevice', '', "'"]
     # remove last two and first words
-    # of the list so it only contains needed information. output is:
+    # of the list so it only contains needed information. OUTPUT is:
     # ['015d2568753c1408\\tdevice', '015d2856d607f015\\tdevice']
-    output = output.split('\\n')[1:-2]
+    OUTPUT = OUTPUT.split('\\n')[1:-2]
 
-    # Now each word in output[] is a serial followed by a '\\t' and other info
+    # Now each word in OUTPUT[] is a serial followed by a '\\t' and other info
     # Cut off everything except the serial, which is from the beginning of the
     # line to the '\\t'.
-    devices = [line.split('\\t')[0] for line in output]
+    DEVICES = [line.split('\\t')[0] for line in OUTPUT]
     # iterate through each device serial number to copy file to it
     # give connection some time to initialize
     time.sleep(.1)  # seconds
-    for device in devices:
-        if device not in devices_with_file:
+    for device in DEVICES:
+        if device not in DEVICES_WITH_FILE:
             # use dictionary to find human-readable
             # device name from given device serial
-            # example: serials_dict['00a2849de'] evaluates to 'Scout 7'
-            device_name = serials_dict[device]
+            # example: SERIALS_DICT['00a2849de'] evaluates to 'Scout 7'
+            device_name = SERIALS_DICT[device]
             # using subprocess, call 'adb push' command
             # which uses Android Debug Bridge (adb) to copy files
             # the -s flag tells adb which device
             # to push to using its serial number
-            subprocess.call(f"adb -s {device} push '{assignment_file_path}' \
+            subprocess.call(f"adb -s {device} push '{ASSIGNMENT_FILE_PATH}' \
                              '/mnt/sdcard/bluetooth/assignments.txt'",
                             shell=True)
 
             if file_load_success(device):
-                devices_with_file.append(device)
+                DEVICES_WITH_FILE.append(device)
 
                 print(f'Loaded assignment.txt file onto tablet ' \
                       f'\'{device_name}\'')
