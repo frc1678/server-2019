11b6c04ba10f6e1c949371218c4d525e077e8201
Apurva Mishra <apurvagyan@gmail.com>
2019-02-16 14:32:27 -0800

    Make file_load_success function use adb shell instead of pulling a copy of the file on to the computer

--- send_assignment_file.py
+++ send_assignment_file.py
@@ -43,41 +43,39 @@ DEVICE_NAMES = {
 ASSIGNMENT_FILE_PATH = utils.create_file_path(
     'data/assignments/assignments.txt')
 
-# Devices that 'assignments.txt' has already been sent to
+# List of devices to which 'assignments.txt' has already been sent
 # This way we know not to load the file onto the device again
 DEVICES_WITH_FILE = []
 
 def file_load_success(device_id):
-    """ Makes sure that (1) the file exists and
-    (2) that file is the same as assignments.txt """
-    # pull tablet's copy of assignments.txt into pulled_copy.txt
-    # then compare pulled_copy.txt to assignments.txt
-    # if they're the same then it was pushed properly
-
-    # remove everything after last slash
-    copy_file_path = ASSIGNMENT_FILE_PATH.split("/")[:-1]
-    copy_file_path.append('pulled_copy.txt')
-    copy_file_path = '/'.join(copy_file_path)
-    # use 'utils.create_file_path()' to create the file if it doesn't exist
-    copy_file_path = utils.create_file_path(copy_file_path, True)
+    """ Makes sure that the file on the tablet
+    is the same as assignments.txt
+    by comparing tablet_data and computer_data """
+    # the 'r' option for open() indicates that
+    # the file will only be used for reading from
+    with open(ASSIGNMENT_FILE_PATH, 'r') as file:
+        computer_data = file.read()
     try:
-        subprocess.call(f'adb -s {device_id} pull /mnt/sdcard/bluetooth/assignments.txt {copy_file_path}',
-                        shell=True, stdout=subprocess.DEVNULL)
-        # the 'r' option for open() indicates that
-        # the file will only be used for reading from
-        with open(copy_file_path, 'r') as file:
-            copy_data = file.read()
-        with open(ASSIGNMENT_FILE_PATH, 'r') as file:
-            assignment_data = file.read()
-        return copy_data == assignment_data
+        # the -s flag tells adb the serial number of which device to use
+        tablet_data = subprocess.check_output(
+            f'adb -s {device_id} shell cat /mnt/sdcard/bluetooth/assignments.txt',
+            shell=True)
     except FileNotFoundError:
         return False
+    # tablet_data is a "byte-like string"
+    # That means when we convert it to a string, it will look like
+    # b"Hello world" so we use [2:-1] to remove the b" at the
+    # beginning and the " at the end
+    # Also, it has \r\n everywhere there should be a newline
+    # instead of an actual newline, so we replace r'\r\n' with '\n'
+    tablet_data = str(tablet_data)[2:-1].replace(r'\r\n', '\n')
+    return tablet_data == computer_data
 
 while True:
     # stores output from 'adb devices' command in var 'OUTPUT'
     # 'adb devices' finds serials of all connected devices (USB cable)
-
     # Here's an example output of 'adb devices':
+
     # List of devices attached
     # 015d2568753c1408	device
     # 015d2856d607f015	device
@@ -86,7 +84,7 @@ while True:
     # makes it weird, so here's what OUTPUT looks like now:
     # "b'List of devices attached\\n015d2568753c1408\\tdevice\\n015d2856d607f015\\tdevice\\n\\n'"
 
-    # now split OUTPUT values at each '\\n' so that OUTPUT looks like:
+    # now split OUTPUT values at each r'\n' so that OUTPUT looks like:
     # ["b'List of devices attached", '015d2568753c1408\\tdevice',
     #  '015d2856d607f015\\tdevice', '', "'"]
     # remove last two and first words
@@ -94,19 +92,15 @@ while True:
     # ['015d2568753c1408\\tdevice', '015d2856d607f015\\tdevice']
     OUTPUT = OUTPUT.split(r'\n')[1:-2]
 
-    # Now each word in OUTPUT[] is a serial followed by a '\\t' and other info
+    # Now each word in OUTPUT[] is a serial followed by a r'\t' and other info
     # Cut off everything except the serial, which is from the beginning of the
-    # line to the '\\t'.
+    # line to the r'\t'.
     DEVICES = [line.split(r'\t')[0] for line in OUTPUT]
-    # iterate through each device serial number to copy file to it
     # give connection some time to initialize
     time.sleep(.1)  # seconds
+    # iterate through each device serial number to copy file to it
     for device in DEVICES:
         if device not in DEVICES_WITH_FILE:
-            # use dictionary to find human-readable
-            # device name from given device serial
-            # example: DEVICE_NAMES['00a2849de'] evaluates to 'Scout 7'
-            device_name = DEVICE_NAMES[device]
             # using subprocess, call 'adb push' command
             # which uses Android Debug Bridge (adb) to copy files
             # the -s flag tells adb which device
@@ -117,5 +111,8 @@ while True:
 
             if file_load_success(device):
                 DEVICES_WITH_FILE.append(device)
-
+                # use dictionary to find human-readable
+                # device name from given device serial
+                # example: DEVICE_NAMES['00a2849de'] evaluates to 'Scout 7'
+                device_name = DEVICE_NAMES[device]
                 print(f'Loaded assignment.txt file onto tablet {device_name}')
