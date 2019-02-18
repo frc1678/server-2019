b7db9072b97c837d5f74cf370bfb9906fb7b89c6
ccsaposs <ccsaposs+github@gmail.com>
2019-02-18 15:15:35 -0800

    Add handling for compressed tempTIMD newline bug

--- server.py
+++ server.py
@@ -95,6 +95,9 @@ def temp_timd_stream_handler(snapshot):
 
     # This saves each tempTIMD in a separate text file.
     for temp_timd_name, temp_timd_value in data.items():
+        # HACK: Remove trailing '\n' (newlines) in compressed tempTIMD
+        # data.  This is a bug in the Scout app.
+        temp_timd_value = temp_timd_value.rstrip('\n')
         # This means that this tempTIMD has been deleted from Firebase
         # and we should delete it from our local copy.
         if temp_timd_value is None:
