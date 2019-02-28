6955aeb7c67b21da7db155df216e919ce3668c95
Apurva Mishra <apurvagyan@gmail.com>
2019-02-27 20:45:18 -0800

    Revert "Update firebase to CVR for field test 1 (#82)"
    This reverts commit 89c3059e5f09e4c1909c0413cd4b193908835f92.

--- firebase_communicator.py
+++ firebase_communicator.py
@@ -8,7 +8,7 @@ def configure_firebase(url=None):
 
     If no URL is given, the default URL is used."""
     if url is None:
-        url = 'scouting-2019-cvr-e4cf3'
+        url = 'field-test-2019'
     config = {
         'apiKey': 'mykey',
         'authDomain': url + '.firebaseapp.com',
