e5c9be8d428c0389848eb7561ef8b4f0535cade1
ccsaposs <ccsaposs+github@gmail.com>
2019-03-05 21:38:37 -0800

    Add TIMD data forwarding

--- forward_tba_data.py
+++ forward_tba_data.py
@@ -57,3 +57,47 @@ for team in RANKINGS:
     }
     save_data(f'teams/{team_number}.json', team_data)
 
+# TIMD and Match data
+MATCH_KEYS = tba_communicator.request_match_keys()
+for match_key in MATCH_KEYS:
+    # 'qm' stands for qualification match
+    # Example 'match_key' formats: '2019caoc_qm29', '2019caoc_qf3m1'
+    if match_key.split('_')[1][:2] == 'qm':
+        match = tba_communicator.request_match(match_key)
+        match_number = match['match_number']
+        teams_by_alliance = {
+            'red': match['alliances']['red']['team_keys'],
+            'blue': match['alliances']['blue']['team_keys'],
+        }
+        for alliance in teams_by_alliance:
+            alliance_score_breakdown = match['score_breakdown'][alliance]
+            teams = [int(team[3:]) for team in teams_by_alliance[alliance]]
+            no_show_teams = []
+            # 'teams' are ordered by driver station
+            # (e.g. in [1678, 3132, 1323], 1678 is driver station 1,
+            # 3132 is driver station 2, and 1323 is driver station 3)
+
+            # TIMD data
+            for driver_station, team_number in enumerate(teams, 1):
+                starting_level = alliance_score_breakdown[
+                    f'preMatchLevelRobot{driver_station}']
+                # Checks if team is a no-show
+                if starting_level == 'None':
+                    no_show_teams.append(team_number)
+                    is_no_show = True
+                    # 'hab_line_crossed' cannot exist if the team is a no-show.
+                    hab_line_crossed = None
+                else:
+                    is_no_show = False
+                    hab_line_crossed = alliance_score_breakdown[
+                        f'habLineRobot{driver_station}']
+                timd_data = {
+                    'driverStation': driver_station,
+                    'startingLevel': starting_level,
+                    'isNoShow': is_no_show,
+                    'crossedHabLine': hab_line_crossed,
+                }
+                # Example TIMD name: '1678Q3' (1678 in match 3)
+                timd_name = f'{team_number}Q{match_number}'
+                save_data(f'timds/{timd_name}.json', timd_data)
+
--- tba_communicator.py
+++ tba_communicator.py
@@ -72,6 +72,12 @@ def make_request(api_url):
         print(f'Request failed with status code {request.status_code}')
         return {}
 
+def request_match(match_key):
+    """Requests data for a single match from the TBA API.
+
+    match_key is a string.  (e.g. '2019caoc_qm29', '2019caoc_qf3m1')"""
+    return make_request(f'match/{match_key}')
+
 def request_rankings():
     """Requests the team rankings for an event from the TBA API."""
     return make_request(f'event/{EVENT_CODE}/rankings')
@@ -79,6 +85,14 @@ def request_rankings():
 def request_matches():
     """Requests the match schedule from the TBA API."""
     return make_request(f'event/{EVENT_CODE}/matches/simple')
+
+def request_match_keys():
+    """Requests match keys (names) from the TBA API.
+
+    Match key format: {event_code}_{match_number}
+    (e.g. '2019caoc_qm29' [qualification match 29],
+    '2019caoc_qf3m1' [quarter finals 3-1])"""
+    return make_request(f'event/{EVENT_CODE}/matches/keys')
 
 def request_teams():
     """Requests the team list from the TBA API."""
