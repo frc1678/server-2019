fef0de8da37e531a9d1f6147213eb221c7e030f7
EThan1678 <ethangtalbert@gmail.com>
2019-03-08 00:33:27 -0800

    Move advanced calculations call

--- server.py
+++ server.py
@@ -263,11 +263,11 @@ while True:
     # Forwards TBA data to Teams, TIMDs, and Matches.
     subprocess.call('python3 forward_tba_data.py', shell=True)
 
-    # Runs advanced calculations for every team in the competition.
-    subprocess.call('python3 make_advanced_calculations.py', shell=True)
-
     # Forwards tempSuper data to Matches and TIMDs.
     subprocess.call('python3 forward_temp_super.py', shell=True)
+
+    # Runs advanced calculations for every team in the competition.
+    subprocess.call('python3 make_advanced_calculations.py', shell=True)
 
     # Uploads data in data queue.
     subprocess.call('python3 upload_data.py', shell=True)
