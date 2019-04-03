db462ef874cc15e59cdd5d490d3987a71084c05e
ccsaposs <ccsaposs+github@gmail.com>
2019-04-02 20:18:02 -0700

    Update Firebase URL

--- firebase_communicator.py
+++ firebase_communicator.py
@@ -3,7 +3,7 @@
 import pyrebase
 # No internal imports
 
-URL = 'scouting-2019-sac-2bdd7'
+URL = 'scouting-2019-avr-cacf9'
 
 def configure_firebase(url=None):
     """Returns a firebase database instance based on a database URL.
