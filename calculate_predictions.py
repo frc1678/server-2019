4217d9df0d54b196365e37333c8b712eff6021aa
EThan1678 <ethangtalbert@gmail.com>
2019-04-04 21:01:56 -0700

    Make dictionaries singular

--- calculate_predictions.py
+++ calculate_predictions.py
@@ -68,7 +68,7 @@ def calculate_chance_climb_rp(team_numbers):
     """Calculates the chance an alliance gets the climb RP (ranking point).
 
     team_numbers are the team_numbers on the alliance."""
-    # Each team to their 'calculatedData' dictionaries
+    # Each team to their 'calculatedData' dictionary
     calculated_data_by_team = {team_number: \
         TEAMS[team]['calculatedData'] for team_number in \
         team_numbers}
