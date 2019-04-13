8a48339011c9be1a1641a078b2b337e4246abdc8
ccsaposs <ccsaposs+github@gmail.com>
2019-04-13 08:15:50 -0700

    Change min_level to an int before calling range

--- calculate_predictions.py
+++ calculate_predictions.py
@@ -106,7 +106,7 @@ def calculate_chance_climb_rp(team_numbers):
                 # (and including) the minimum level and 3.
                 level_success_rates = {level: \
                     available_teams[team][str(level)] for level in \
-                    range(min_level, 3+1)}
+                    range(int(min_level), 3+1)}
                 # Gets the max success rate out of all the levels
                 max_successes[team] = max(level_success_rates.values())
             # After each team's highest success rate is taken, the
