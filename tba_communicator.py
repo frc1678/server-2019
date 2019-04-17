d7d3e2888995092123a23bddf5e4b4e26b1a76e1
Carl Csaposs <32380359+ccsaposs@users.noreply.github.com>
2019-04-16 21:53:24 -0700

    Add 60 second cache age to forward_tba_data.py (#185)
    * Add 'acceptable_cache_age' to tba_communicator.py
    
    * Add 60 second cache age to forward_tba_data.py

--- tba_communicator.py
+++ tba_communicator.py
@@ -16,12 +16,14 @@ with open(utils.create_file_path('data/api_keys/tba_key.txt')) as file:
 # Many file editors will automatically add a newline at the end of files.
 API_KEY = API_KEY.rstrip('\n')
 
-def make_request(api_url, show_output=True):
+def make_request(api_url, show_output=True, acceptable_cache_age=0):
     """Sends a single web request to the TBA API v3 and caches result.
 
     api_url is the url of the API request (the path after '/api/v3')
     show_output shows print statements about the status of the
-    request.  Defaults to True"""
+    request.  Defaults to True
+    acceptable_cache_age is the maximum age (in seconds) of data that
+    can be pulled from the cache."""
     base_url = 'https://www.thebluealliance.com/api/v3/'
     full_url = base_url + api_url
     request_headers = {'X-TBA-Auth-Key': API_KEY}
@@ -38,6 +40,12 @@ def make_request(api_url, show_output=True):
     except FileNotFoundError:
         cached_requests = {}
 
+    # Returns the cached request if it was pulled within the
+    # 'acceptable_cache_age' limit.
+    last_request_time = cached_requests.get(api_url, {}).get('last_requested', 0)
+    if (time.time() - last_request_time) < acceptable_cache_age:
+        return cached_requests[api_url]['data']
+
     # 'cache_last_modified' is the time that the data in the cache was
     # published to TBA's API.
     cache_last_modified = cached_requests.get(api_url, {}).get('last_modified')
@@ -48,6 +56,7 @@ def make_request(api_url, show_output=True):
         print(f'Retrieving data from TBA...\nURL: {api_url}')
     while True:
         try:
+            request_time = time.time()
             request = requests.get(full_url, headers=request_headers)
         except requests.exceptions.ConnectionError:
             # Errors will always be shown, even if 'show_output' is False.
@@ -66,6 +75,7 @@ def make_request(api_url, show_output=True):
     elif request.status_code == 200:
         # Updates local cache
         cached_requests[api_url] = {
+            'last_requested': request_time,
             'last_modified': request.headers['Last-Modified'],
             'data': request.json(),
         }
@@ -78,28 +88,28 @@ def make_request(api_url, show_output=True):
         print(f'Request failed with status code {request.status_code}')
         return {}
 
-def request_match(match_key, show_output=True):
+def request_match(match_key, show_output=True, acceptable_cache_age=0):
     """Requests data for a single match from the TBA API.
 
     match_key is a string.  (e.g. '2019caoc_qm29', '2019caoc_qf3m1')"""
-    return make_request(f'match/{match_key}', show_output)
+    return make_request(f'match/{match_key}', show_output, acceptable_cache_age)
 
-def request_rankings(show_output=True):
+def request_rankings(show_output=True, acceptable_cache_age=0):
     """Requests the team rankings for an event from the TBA API."""
-    return make_request(f'event/{EVENT_CODE}/rankings', show_output)
+    return make_request(f'event/{EVENT_CODE}/rankings', show_output, acceptable_cache_age)
 
-def request_matches(show_output=True):
+def request_matches(show_output=True, acceptable_cache_age=0):
     """Requests the match schedule from the TBA API."""
-    return make_request(f'event/{EVENT_CODE}/matches/simple', show_output)
+    return make_request(f'event/{EVENT_CODE}/matches/simple', show_output, acceptable_cache_age)
 
-def request_match_keys(show_output=True):
+def request_match_keys(show_output=True, acceptable_cache_age=0):
     """Requests match keys (names) from the TBA API.
 
     Match key format: {event_code}_{match_number}
     (e.g. '2019caoc_qm29' [qualification match 29],
     '2019caoc_qf3m1' [quarter finals 3-1])"""
-    return make_request(f'event/{EVENT_CODE}/matches/keys', show_output)
+    return make_request(f'event/{EVENT_CODE}/matches/keys', show_output, acceptable_cache_age)
 
-def request_teams(show_output=True):
+def request_teams(show_output=True, acceptable_cache_age=0):
     """Requests the team list from the TBA API."""
-    return make_request(f'event/{EVENT_CODE}/teams/simple', show_output)
+    return make_request(f'event/{EVENT_CODE}/teams/simple', show_output, acceptable_cache_age)
