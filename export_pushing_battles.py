dd6b5638ef9c13195412cad09520cdfc1f7eb1f1
Carl Csaposs <32380359+ccsaposs@users.noreply.github.com>
2019-04-18 08:37:21 -0500

    Add export_pushing_battles.py (#191)
    Collects and exports all Super Scout pushing battles to a CSV file.

---
+++ export_pushing_battles.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Collects and exports all Super Scout pushing battles to a CSV file."""
+# External imports
+import csv
+import os
+# Internal imports
+import decompressor
+import utils
+
+TEMP_SUPER_FILES = os.listdir(utils.create_file_path('data/cache/temp_super'))
+PUSHING_BATTLES = []
+for temp_super_file in TEMP_SUPER_FILES:
+    with open(utils.create_file_path(
+            f'data/cache/temp_super/{temp_super_file}'), 'r') as file:
+        compressed_temp_super = file.read()
+    decompressed_pushing_battles = \
+        decompressor.decompress_temp_super_pushing_battles(
+            compressed_temp_super)
+    PUSHING_BATTLES += decompressed_pushing_battles
+
+CSV_HEADERS = ['matchNumber', 'winner', 'loser', 'winMarginIsLarge']
+
+with open(utils.create_file_path('data/exports/pushing-battles.csv'), 'w') as file:
+    CSV_WRITER = csv.DictWriter(file, fieldnames=CSV_HEADERS)
+    CSV_WRITER.writeheader()
+
+    for pushing_battle in PUSHING_BATTLES:
+        CSV_WRITER.writerow(pushing_battle)
